28fd13a6d824ec6ac93807eb2f791e9e579a96fc
JeffDT11 <delatorre.jeffrey02@gmail.com>
2023-11-24 09:59:04 +0800

    ver9
    separate announcement pages
    issues: not yet working

---
+++ Backend/student_readmissiondate.php
@@ -0,0 +1,64 @@
+<?php
+
+include("config.php");
+include("studentcover.php");
+
+// Check if the user is a student member, otherwise redirect them
+if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] !== 'student') {
+    header("Location: loginpage.php");
+    exit();
+}
+
+// Retrieve the student's information from the users table
+$studentId = $_SESSION['user_id'];
+$stmtUser = $conn->prepare("SELECT * FROM users WHERE id = ?");
+$stmtUser->bind_param("i", $studentId);
+$stmtUser->execute();
+$resultUser = $stmtUser->get_result();
+$studentData = $resultUser->fetch_assoc();
+
+// Retrieve the admission data based on the user's email
+$email = $studentData['email'];
+$stmtAdmission = $conn->prepare("SELECT * FROM admission_data WHERE email = ?");
+$stmtAdmission->bind_param("s", $email);
+$stmtAdmission->execute();
+$resultAdmission = $stmtAdmission->get_result();
+$admissionData = $resultAdmission->fetch_assoc();
+
+// Display the student's and admission data
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Profile</title>
+    <link rel="icon" href="assets/images/BSU Logo1.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/student.css" />
+    <!-- Boxicons -->
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
+    <script src="assets\js\jspdf.min.js"></script>
+<!-- Include the pdf.js library -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    </head>
+
+    <body>
+            <section id="content">
+                <main>
+                    <div id="readmission-announce" style="display: none;">
+                        <div class="table-data">
+                            <div class="order">
+                                <div class="head">
+                                    <h3>Readmission Date</h3>
+                                </div>
+                                <div>Readmission will start January 1- to 29</div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </section>
+    </body>
+</html>
