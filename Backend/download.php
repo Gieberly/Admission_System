c21d33c997c345675720251e9499de69f111d9e5
Seung01 <126255298+Seung01@users.noreply.github.com>
2024-01-22 00:31:33 +0800

    Version00003
    Download PDF Trial#2

--- Backend/download.php
+++ Backend/download.php
@@ -135,171 +135,172 @@ $conn->close();
           </p>
         <p class="head_information">BACKGROUND INFORMATION OF APPLICANT</p>
         <p class="personal_information">Personal Information</p>
-        <div class="form-container">
+
+        <div class="form-container1">
           <!-- Full name -->
           <div class="form-group">
             <label class="small-label" for="applicant_name">Complete Name</label>
-            <input name="applicant_name" class="input" id="applicant_name" value="<?php echo $admissionData['applicant_name']; ?>">
+            <input name="applicant_name" class="input" id="applicant_name" value="<?php echo $admissionData['applicant_name']; ?>" readonly>
           </div>
+
+          <!-- Birthplace -->
+          <div class="form-group">
+            <label class="small-label" for="birthplace">Birthplace</label>
+            <input name="birthplace" class="input" id="birthplace" value="<?php echo $admissionData['birthplace']; ?>" readonly>
+          </div>
+        </div>
+
+        <div class="form-container2">
           <!-- Sex at Birth -->
           <div class="form-group">
             <label class="small-label" for="gender">Sex at birth</label>
-            <input name="gender" class="input" id="gender" value="<?php echo $admissionData['gender']; ?>">
+            <input name="gender" class="input" id="gender" value="<?php echo $admissionData['gender']; ?>" readonly>
           </div>
           <!-- Birthdate -->
           <div class="form-group">
             <label class="small-label" for="birthdate">Birthdate</label>
-            <input name="birthdate" class="input" id="birthdate" value="<?php echo $admissionData['birthdate']; ?>">
+            <input name="birthdate" class="input" id="birthdate" value="<?php echo $admissionData['birthdate']; ?>" readonly>
           </div>
         <!-- Age -->
         <div class="form-group">
             <label class="small-label" for="age">Age</label>
-            <input name="age" class="input" id="age" value="<?php echo $admissionData['age']; ?>">
-          </div>
-          </div>
-
-        <p class="birthplace"></p>
-        <div class="form-container">
-        <!-- Birthplace -->
-          <div class="form-group">
-            <label class="small-label" for="birthplace">Birthplace</label>
-            <input name="birthplace" class="input" id="birthplace" value="<?php echo $admissionData['birthplace']; ?>">
+            <input name="age" class="input" id="age" value="<?php echo $admissionData['age']; ?>" readonly>
           </div>
           <!-- civil status -->
           <div class="form-group">
             <label class="small-label" for="civil_status">Civil Status</label>
-            <input name="civil_status" class="input" id="civil_status" value="<?php echo $admissionData['civil_status']; ?>">
+            <input name="civil_status" class="input" id="civil_status" value="<?php echo $admissionData['civil_status']; ?>" readonly>
           </div>
           <!-- Citizenship -->
           <div class="form-group">
             <label class="small-label" for="citizenship">Citizenship</label>
-            <input name="citizenship" class="input" id="citizenship" value="<?php echo $admissionData['citizenship']; ?>">
+            <input name="citizenship" class="input" id="citizenship" value="<?php echo $admissionData['citizenship']; ?>" readonly>
           </div>
           <!-- Nationality-->
           <div class="form-group">
             <label class="small-label" for="nationality">Nationality</label>
-            <input name="nationality" class="input" id="nationality" value="<?php echo $admissionData['nationality']; ?>">
+            <input name="nationality" class="input" id="nationality" value="<?php echo $admissionData['nationality']; ?>" readonly>
           </div>
         </div>
 
         <p class="personal_information">Permanent Home Address</p>
-        <div class="form-container">
 
+        <div class="form-container3">
           <div class="form-group">
             <label class="small-label" for="permanent_address">Address</label>
-            <input name="permanent_address" class="input" id="permanent_address" value="<?php echo $admissionData['permanent_address']; ?>">
+            <input name="permanent_address" class="input" id="permanent_address" value="<?php echo $admissionData['permanent_address']; ?>" readonly>
           </div>
           <!-- zip-code -->
           <div class="form-group">
             <label class="small-label" for="zip_code">Zip Code</label>
-            <input name="zip_code" class="input" id="zip_code" value="<?php echo $admissionData['zip_code']; ?>">
+            <input name="zip_code" class="input" id="zip_code" value="<?php echo $admissionData['zip_code']; ?>" readonly>
           </div>
         </div>
 
         <p class="personal_information">Contact Information</p>
-        <div class="form-container">
+        <div class="form-container4">
           <!-- Telephone/Mobile No -->
           <div class="form-group">
             <label class="small-label" for="phone">Telephone/Mobile No.</label>
-            <input name="phone" class="input" id="phone" value="<?php echo $admissionData['phone']; ?>">
+            <input name="phone" class="input" id="phone" value="<?php echo $admissionData['phone']; ?>" readonly>
           </div>
 
           <!-- Facebook Account Name -->
           <div class="form-group">
             <label class="small-label" for="facebook">Facebook Account Name</label>
-            <input name="facebook" class="input" id="facebook" value="<?php echo $admissionData['facebook']; ?>">
+            <input name="facebook" class="input" id="facebook" value="<?php echo $admissionData['facebook']; ?>" readonly>
           </div>
           <!--Email Address -->
           <div class="form-group">
             <label class="small-label" for="email">Email Address</label>
-            <input name="email" class="input" id="email" value="<?php echo $admissionData['email']; ?>">
+            <input name="email" class="input" id="email" value="<?php echo $admissionData['email']; ?>" readonly>
           </div>
-
         </div>
-        <p class="personal_information">Contact Person(s) in Case of Emergency</p>
 
-        <div class="form-container">
+        <p class="personal_information">Contact Person(s) in Case of Emergency</p>
+        <div class="form-container4">
           <!-- Contact Person 1 -->
           <div class="form-group">
             <label class="small-label" for="contact_person_1">Contact Person</label>
-            <input name="contact_person_1" class="input" id="contact_person_1" value="<?php echo $admissionData['contact_person_1']; ?>">
+            <input name="contact_person_1" class="input" id="contact_person_1" value="<?php echo $admissionData['contact_person_1']; ?>" readonly>
           </div>
           <div class="form-group">
             <label class="small-label" for="contact_person_1_mobile">Mobile Number</label>
-            <input name="contact_person_1_mobile" class="input" id="contact_person_1_mobile" value="<?php echo $admissionData['contact_person_1_mobile']; ?>">
+            <input name="contact_person_1_mobile" class="input" id="contact_person_1_mobile" value="<?php echo $admissionData['contact_person_1_mobile']; ?>" readonly>
           </div>
           <div class="form-group">
-            <label class="relationship-label" for="relationship_1">Relationship with Contact Person</label>
-            <input name="relationship_1" class="input" id="relationship_1" value="<?php echo $admissionData['relationship_1']; ?>">
+            <label class="small-label" for="relationship_1">Relationship with Contact Person</label>
+            <input name="relationship_1" class="input" id="relationship_1" value="<?php echo $admissionData['relationship_1']; ?>" readonly>
           </div>
         </div>
-        <div class="form-container">
+        <div class="form-container4">
           <!-- Contact Person 2 -->
           <div class="form-group">
             <label class="small-label" for="contact_person_2">Contact Person</label>
-            <input name="contact_person_2" class="input" id="contact_person_2" value="<?php echo $admissionData['contact_person_2']; ?>">
+            <input name="contact_person_2" class="input" id="contact_person_2" value="<?php echo $admissionData['contact_person_2']; ?>" readonly>
           </div>
           <div class="form-group">
             <label class="small-label" for="contact_person_2_mobile">Mobile Number</label>
-            <input name="contact_person_2_mobile" class="input" id="contact_person_2_mobile" value="<?php echo $admissionData['contact_person_2_mobile']; ?>">
+            <input name="contact_person_2_mobile" class="input" id="contact_person_2_mobile" value="<?php echo $admissionData['contact_person_2_mobile']; ?>" readonly>
           </div>
           <div class="form-group">
-            <label class="relationship-label" for="relationship_2">Relationship with Contact Person</label>
-            <input name="relationship_2" class="input" id="relationship_2" value="<?php echo $admissionData['relationship_2']; ?>">
+            <label class="small-label" for="relationship_2">Relationship with Contact Person</label>
+            <input name="relationship_2" class="input" id="relationship_2" value="<?php echo $admissionData['relationship_2']; ?>" readonly>
           </div>
         </div>
 
         <p class="personal_information">Academic Classification</p>
-        <div class="form-container">
+        <div class="form-container6">
           <!-- Academic Classification -->
           <div class="form-group">
             <label class="small-label" for="academic_classification">Academic Classification</label>
-            <input name="academic_classification" class="input" id="academic_classification" value="<?php echo $admissionData['academic_classification']; ?>">
-          </div>
-        </div>
-        <p class="personal_information">Academic Background </p>
-        <div class="form-container">
-          <!-- Academic Background -->
-          <div class="form-group">
-            <label class="small-label" for="high_school_name_address" style="white-space: nowrap;">High School/Senior High School</label>
-            <input name="high_school_name_address" class="input" id="high_school_name_address" value="<?php echo $admissionData['high_school_name_address']; ?>">
-
-          </div>
-        </div>
-        <div class="form-container">
-          <div class="form-group">
-            <label class="small-label" for="als_pept_name_address" style="white-space: nowrap;">ALS/PEPT was taken:</label>
-            <input name="als_pept_name_address" class="input" id="als_pept_name_address" value="<?php echo $admissionData['als_pept_name_address']; ?>">
-          </div>
-        </div>
-        <div class="form-container">
-          <div class="form-group">
-            <label class="small-label" for="college_name_address">College/University:</label>
-            <input name="college_name_address" class="input" id="college_name_address" value="<?php echo $admissionData['college_name_address']; ?>">
-        </div>
-        <div class="form-container">
-          <div class="form-group">
-            <label class="small-label" for="lrn" style="white-space: nowrap;">Learner's Reference Number</label>
-            <input name="lrn" class="input" id="lrn" value="<?php echo $admissionData['lrn']; ?>">
+            <input name="academic_classification" class="input" id="academic_classification" value="<?php echo $admissionData['academic_classification']; ?>" readonly>
           </div>
           <div class="form-group">
             <label class="small-label" for="degree_applied">Degree</label>
             <!-- Display the selected program in this input field -->
-            <input name="degree_applied" class="input" id="degree_applied" value="<?php echo $admissionData['degree_applied']; ?>">
+            <input name="degree_applied" class="input" id="degree_applied" value="<?php echo $admissionData['degree_applied']; ?>" readonly>
           </div>
           <div class="form-group">
             <label class="small-label" for="nature_of_degree" style="white-space: nowrap;">Nature of degree</label>
-            <input name="nature_of_degree" class="input" id="nature_of_degree" value="<?php echo $admissionData['nature_of_degree']; ?>">
+            <input name="nature_of_degree" class="input" id="nature_of_degree" value="<?php echo $admissionData['nature_of_degree']; ?>" readonly>
           </div>
         </div>
-
-        <p class="attestation-head" style="text-align: center;">ATTESTATION AND CONSENT</p>
+        <p class="personal_information">Academic Background </p>
+        <div class="form-container1">
+          <!-- Academic Background -->
+          <div class="form-group">
+            <label class="small-label" for="high_school_name_address" style="white-space: nowrap;">High School/Senior High School</label>
+            <input name="high_school_name_address" class="input" id="high_school_name_address" value="<?php echo $admissionData['high_school_name_address']; ?>" readonly>
+          </div>
+          <div class="form-group">
+            <label class="small-label" for="college_name_address">College/University:</label>
+            <input name="college_name_address" class="input" id="college_name_address" value="<?php echo $admissionData['college_name_address']; ?>" readonly>
+        </div>
+        </div>
+        <div class="form-container5">
+          <div class="form-group">
+            <label class="small-label" for="als_pept_name_address" style="white-space: nowrap;">ALS/PEPT was taken:</label>
+            <input name="als_pept_name_address" class="input" id="als_pept_name_address" value="<?php echo $admissionData['als_pept_name_address']; ?>" readonly>
+          </div>
+          <div class="form-group">
+            <label class="small-label" for="lrn" style="white-space: nowrap;">Learner's Reference Number</label>
+            <input name="lrn" class="input" id="lrn" value="<?php echo $admissionData['lrn']; ?>" readonly>
+          </div>
+        </div>
+        <p class="attestation-head">ATTESTATION AND CONSENT</p>
         <p class="attestation-note"><i>I affirm that I have read and understood all the instructions contained in this acknowledgement 
             slip and that the information supplied are true, complete and accurate. I promise to abide by the rules and regulations 
             of the admission process of Benguet State University. I am aware that any information I have concealed, falsely given 
             and/or withheld is enough basis for the invalidation/cancellation of my application. I have understood the Data Privacy 
             Notice above and freely give my consent to the legitimate use of my personal data by the University thru my signature 
             which I have affixed herein.</i></p><br><br>
+
+        <div class="student_signature">
+            <p>Signature Over Printed Name</p>
+        </div><br>
+
+
 </div>
 </body>
 
@@ -311,9 +312,9 @@ $conn->close();
         // html2pdf().from(element).save()
         var element = document.getElementById("DownloadForm");
         var opt = {
-            margin: 6.35,
+            margin: 0.39,
             filename: 'Admission Form.pdf',
-            jsPDF: {unit: 'mm', format: [215.9, 330.2], orientation: 'portrait'},
+            jsPDF: {unit: 'in', format: [8.5, 13], orientation: 'portrait'},
             pagebreak: { before: '#page-containerr', avoid: '.avoid-this' }
         };
         html2pdf().set(opt).from(element).save();
