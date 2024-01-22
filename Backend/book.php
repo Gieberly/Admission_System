250ed2861082d3b995422f685885e335b38f26d9
Zenon2000 <puylongarjie@gmail.com>
2024-01-23 04:30:19 +0800

    booking_update

--- Backend/book.php
+++ Backend/book.php
@@ -1,53 +1,52 @@
 <?php
+include ('config.php');
+
 if(isset($_GET['date'])){
     $date = $_GET['date'];
 }
 
-if(isset($_POST['submit'])){
+if(isset($_POST['submit_appo'])){
+    $fname = $_POST['FIRSTNAME'];
+    $mname = $_POST['MIDDLENAME'];
+    $lname = $_POST['LASTNAME'];
+    $email = $_POST['EMAIL'];
+    $autonum = $_POST['AUTONUM'];
 
-        $fname =$_POST['FIRSTNAME'];
-        $mname =$_POST['MIDDLENAME'];
-        $lname =$_POST['LASTNAME'];
-        $email =$_POST['EMAIL'];
-		$AUTONUM=$_POST['AUTONUM'];
-        $conn = new mysqli('localhost','root','','bookingsysystem');
+    $query = "INSERT INTO appointments (FIRSTNAME, MIDDLENAME, LASTNAME, EMAIL, DATE, AUTONUM) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('ssssss', $fname, $mname, $lname, $email, $date, $autonum);
 
-        $sql ="INSERT INTO bookings_record(FIRSTNAME,MIDDLENAME,LASTNAME,EMAIL,DATE,AUTONUM)VALUES('$fname','$mname','$lname','$email','$date','$AUTONUM')";
-        
-        if($conn->query($sql)){
-            $message = "<div class='alert alert-success'>Booking Successfull</div>";
-        }else{
-            $message = "<div class='alert alert-danger'>Booking was not Successfull</div>";
-        }
+    if($stmt->execute()){
+        $message = "<div class='alert alert-success'>Booking Successful</div>";
+    } else {
+        $message = "<div class='alert alert-danger'>Booking was not Successful</div>";
+    }
+    $stmt->close();
 }
-
 ?>
 <?php 
 $a = mt_rand(100000,999999); 
-
-for ($i = 0; $i<6; $i++) 
-{
+for ($i = 0; $i<6; $i++) {
    $a .= mt_rand(0,9);
-}?>
-
+}
+?>
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Booking System</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/main.css">
-  </head>
-
-  <body>
+</head>
+<body>
     <div class="container">
-        <h1 class="text-center alert alert-danger" style="background:#2ecc71;border:none;color:#fff"> Book for Date: <?php echo date('m/d/Y', strtotime($date)) ;?></h1>
+        <h1 class="text-center alert alert-danger" style="background:#2ecc71;border:none;color:#fff"> Book for Date: <?php echo date('m/d/Y', strtotime($date)); ?></h1>
         <div class="row">
             <div class="col-md-12">
                 <?php echo isset($message)?$message:'';?>
-                <form action="" method="POST" autocomplete="off">
+                <form action="" msethod="POST" autocomplete="off">
                     <div class="form-group">
                         <label for=""> FIRST NAME</label>
                         <input type="text" class="form-control" name="FIRSTNAME" required>
@@ -65,8 +64,7 @@ for ($i = 0; $i<6; $i++)
                         <label for=""> EMAIL</label>
                         <input type="email" class="form-control" name="EMAIL" required>
                     </div>
-
-                    <button type="submit" name="submit" class="btn btn-primary"> Submit </button>
+                    <button type="submit" name="submit_appo" class="btn btn-primary"> Submit </button>
                     <a href="index.php" class="btn btn-success">Back</a>
                 </form>
             </div>
@@ -74,5 +72,5 @@ for ($i = 0; $i<6; $i++)
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  </body>
+</body>
 </html>
